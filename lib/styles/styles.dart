e850bdce62184c8e519e6e3fae2e76182224dd01
Erik Silva Maia <erikm384@gmail.com>
2023-06-09 19:33:48 -0300

    remove files useles

--- lib/styles/styles.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Styles {
-  TextStyle textNormal = const TextStyle(color: Colors.black54, fontSize:20);
-  TextStyle textBold = const TextStyle(color: Colors.black87, fontSize:30);
-  
-}

3574af3c41b442ed5e080c67abd6c712164d7dfb
Erik Silva Maia <erikm384@gmail.com>
2023-06-09 04:43:44 -0300

    feat: include model in context

--- api/DataBase/ContextBase.cs
+++ api/DataBase/ContextBase.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.DataBase;
 public class ContextBase : DbContext
 {
-
+    public DbSet<LocalModel>? Local { get; set; }
+    public DbSet<ReservationModel>? Reservation { get; set; }
+    public DbSet<TravelModel>? Travel { get; set; }
+    public DbSet<UserModel>? Users { get; set; }
 }
 
